93e9ec96cbd5a1231adade9d80fb65132b70579d
HQ1\ahassan <abubakar.hassan@nakisa.com>
2022-10-18 15:34:02 +0500

    Project Created

---
+++ src/main/java/com/example/EducationalSite/EducationalSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.EducationalSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EducationalSite/EducationalSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EducationalSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
