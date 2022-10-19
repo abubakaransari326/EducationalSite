25c9f74722bc2e87a6a107aff8bf5617707dbe59
HQ1\ahassan <abubakar.hassan@nakisa.com>
2022-10-19 20:42:25 +0500

    Added methods to Student Service

--- src/main/java/com/example/EducationalSite/services/StudentService.java
+++ src/main/java/com/example/EducationalSite/services/StudentService.java
@@ -1,4 +1,19 @@
 package com.example.EducationalSite.services;
 
+import com.example.EducationalSite.models.Course;
+import com.example.EducationalSite.models.Student;
+import com.example.EducationalSite.repositories.CourseRepository;
+import com.example.EducationalSite.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class StudentService {
+    @Autowired
+    StudentRepository studentRepository;
+
+    public ResponseEntity<Student> createStudent(Student student)
+    {
+        return new ResponseEntity(studentRepository.save(student), HttpStatus.OK);
+    }
 }
