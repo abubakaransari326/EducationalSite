62c4ba1156ddc8d051a267a8a8a37705bb8b3a89
HQ1\ahassan <abubakar.hassan@nakisa.com>
2022-10-21 21:11:27 +0500

    Student Controller and Service class completed. End points for all student operations are now working

--- src/main/java/com/example/EducationalSite/controllers/StudentController.java
+++ src/main/java/com/example/EducationalSite/controllers/StudentController.java
@@ -1,4 +1,48 @@
 package com.example.EducationalSite.controllers;
 
+import com.example.EducationalSite.models.Student;
+import com.example.EducationalSite.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    //Create a new Student
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student student)
+    {
+        return studentService.createStudent(student);
+    }
+
+    //Update an already existing student
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable("id") long id, @RequestBody Student student)
+    {
+        return studentService.updateStudent(id, student);
+    }
+
+    //Delete a student
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable("id") long id) {
+        return studentService.deleteStudent(id);
+    }
+
+    //View details of a student
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") long id) {
+        return studentService.getStudentById(id);
+    }
+
+    //List all students
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return studentService.getAllStudents();
+    }
 }
--- src/main/java/com/example/EducationalSite/services/StudentService.java
+++ src/main/java/com/example/EducationalSite/services/StudentService.java
@@ -1,13 +1,17 @@
 package com.example.EducationalSite.services;
 
-import com.example.EducationalSite.models.Course;
 import com.example.EducationalSite.models.Student;
-import com.example.EducationalSite.repositories.CourseRepository;
 import com.example.EducationalSite.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class StudentService {
     @Autowired
     StudentRepository studentRepository;
@@ -15,5 +19,53 @@ public class StudentService {
     public ResponseEntity<Student> createStudent(Student student)
     {
         return new ResponseEntity(studentRepository.save(student), HttpStatus.OK);
+    }
+
+    //Update an already existing student
+    public ResponseEntity<Student> updateStudent(long id, Student student)
+    {
+        Optional<Student> studentData = studentRepository.findById(id);
+
+        if (studentData.isPresent()) {
+            Student _student = studentData.get();
+            _student.setFirstName(student.getFirstName());
+            _student.setLastName(student.getLastName());
+            _student.setCourses(student.getCourses());
+            return new ResponseEntity<>(studentRepository.save(_student), HttpStatus.OK);
+
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //Delete a student
+    public ResponseEntity<HttpStatus> deleteStudent(long id) {
+        Student studentToDelete=studentRepository.findById(id).get();
+        try {
+            studentRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //View details of a student
+    public ResponseEntity<Student> getStudentById(long id) {
+        Optional<Student> studentData = studentRepository.findById(id);
+        return studentData.map(student -> new ResponseEntity<>(student, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    //List all students
+    public ResponseEntity<List<Student>> getAllStudents() {
+        try {
+            List<Student> students = new ArrayList<>(studentRepository.findAll());
+            if (students.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(students, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
